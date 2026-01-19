acba1f647a321e4f897639c59718af2eb590690e
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-01-19 11:31:24 +0100

    chore: nx 21.2 update - angular 20

--- apps/docs/src/app/app.config.ts
+++ apps/docs/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
 };
